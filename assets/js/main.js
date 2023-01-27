3b1e7971e0e1600ab74ef3e3bcc6f4928eb1f402
Fabian Ramirez <framirezvidal@gmail.com>
2023-01-27 17:18:13 -0300

    Estilo y Lista V1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,47 +2,63 @@ let contacts = []
 
 const contactsKey = "contacts"
 
+const updateContact = (index) => {
+    const newName = document.getElementById('name').value
+    contacts[index] = newName
+    localStorage.setItem(contactsKey, JSON.stringify(contacts))
+    window.location.reload()
+}
+const deleteContact = (index) => {
+    contacts.splice(index, 1)
+    localStorage.setItem(contactsKey, JSON.stringify(contacts))
+    window.location.reload()
+}
+
 const addContact = (e) => {
     e.preventDefault()
     const name = document.getElementById("name").value
     contacts.push(name)
     localStorage.setItem(contactsKey, JSON.stringify(contacts))
-    
+    window.location.reload()
+
 }
 
 const getContacts = () => {
-    const contactListString =
-        localStorage.getItem(contactsKey)
-    contacts = JSON.parse(contactListString)
-    console.log(contacts);
+    const contactsListString = localStorage.getItem(contactsKey)
+
+    contacts = JSON.parse(contactsListString)
+    
+
+    const namesList = document.getElementById('lista-nombres')
+
+    contacts.forEach((name, index) => {
+        const nodeName = document.createElement('p')
+        const updateButton = document.createElement('button')
+        updateButton.textContent = `Actualizar ${index + 1}`
+        updateButton.setAttribute('onclick', `updateContact(${index})`)
+
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = `Eliminar ${index + 1}`
+
+        deleteButton.setAttribute('onclick', `deleteContact(${index})`)
+
+        nodeName.textContent = `${index + 1}: ${name}`
+        namesList.appendChild(nodeName)
+        namesList.appendChild(updateButton)
+        namesList.appendChild(deleteButton)
+
+    })
 }
 
-const nameList = document.getElementById("lista-nombres")
-
-contacts.forEach((name,index) => {
-    const nodeName = document.createElement("p")
-    const updateButton = document.createElement("button")
-    updateButton
-    nodeName.textContent = "${index + 1}:${name}"
-    nameList.appendChild(nodeName)
-    nameList.appendChild(updateButton)
-    nameList.appendChild(deleteButton)
-})
-
-const updateContact = (id, name) => { }
-
-const deleteContact = (index) => {
-    contacts.splice(index, 1)
-}
+// Cargar los datos del localstorage
 
 getContacts()
 
-const addButton = document.getElementById('add-button')
+const addButton = document.getElementById( 'add-button' )
 
-const form = document.getElementById("form")
+const form = document.getElementById( 'form' )
 
-const name = document.getElementById("name")
+form.addEventListener( 'submit', addContact )
 
-form.addEventListener("submit", addContact)
-
-addButton.addEventListener('click', addContact)
+addButton.addEventListener( 'click', addContact )
